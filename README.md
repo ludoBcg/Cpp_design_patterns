e648019e3a83eaf3486ac8ed83478165e0223a09
ludoBcg <blache.ludovic@gmail.com>
2023-06-25 19:49:56 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Cpp_design_patterns
 Example code for various C++ design patterns
+
+Provided Cmake scripts generates a VisualStudio solution with one project for each example.
+Implemented with VS2022 and C++20
+
+Sources:
+https://en.wikipedia.org/wiki/Design_Patterns
+https://refactoring.guru/design-patterns/cpp
+https://en.wikibooks.org/wiki/C%2B%2B_Programming/Code/Design_Patterns
---
+++ pimpl.cpp
@@ -0,0 +1,77 @@
+/*********************************************************************************************************************
+ *
+ * class_design.cpp
+ *
+ * Cpp_design_patterns
+ * Ludovic Blache
+ *
+ *********************************************************************************************************************/
+
+// See https://en.cppreference.com/w/cpp/language/pimpl
+
+#include <iostream>
+#include <memory>
+
+
+/*------------------------------------------------------------------------------------------------------------+
+|                                                   CIRCLE.H                                                  |
++-------------------------------------------------------------------------------------------------------------*/
+
+class Circle
+{
+
+    private:
+        class impl; // not defined here
+        std::unique_ptr<impl> pImpl;
+
+    public:
+        // Public API:
+        void draw();
+        void translate();
+ 
+        // Defined in the implementation file:
+        Circle();
+        explicit Circle(int);
+        ~Circle();
+};
+ 
+ 
+/*------------------------------------------------------------------------------------------------------------+
+|                                                  CIRCLE.CPP                                                 |
++-------------------------------------------------------------------------------------------------------------*/
+
+class Circle::impl
+{
+    public:
+        // Actual implementation of functions
+        void draw(const Circle& c)      { std::cout << "draw a circle of radius " << m_radius << std::endl; }
+        void translate(const Circle& c) { std::cout << "translate a circle of radius " << m_radius << std::endl; }
+ 
+        impl() : m_radius(0) {}
+        impl(int n) : m_radius(n) {}
+
+    private:
+        int m_radius; // private data
+};
+ 
+void Circle::draw()      { pImpl->draw(*this); }
+void Circle::translate() { pImpl->translate(*this); }
+
+Circle::Circle() : pImpl(std::make_unique<impl>()) {}
+Circle::Circle(int n) : pImpl(std::make_unique<impl>(n)) {}
+Circle::~Circle() = default;
+
+
+/*------------------------------------------------------------------------------------------------------------+
+|                                                      MAIN                                                   |
++-------------------------------------------------------------------------------------------------------------*/
+
+int main()
+{
+    Circle c1;
+    Circle c2(10);
+    c1.draw();
+    c2.draw();
+    c1.translate();
+    c2.translate();
+}
